3ca9252fde8396b41b4e07a6f11379bca7ec5625
akhtar2344 <akthar.fattan05@gmail.com>
2025-11-16 17:21:46 +0700

    Add lecturer dashboard and update routes

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,13 @@ use App\Http\Controllers\ShowController;
 | Web Routes
 |--------------------------------------------------------------------------
 */
+
+/* -------------------------------------------------------------
+| LECTURER SECTION 
+|--------------------------------------------------------------*/
+Route::prefix('lecturer')->name('lecturer.')->group(function () {
+    // Halaman "root" lecturer (kalau ada landing khusus)
+    Route::view('/', 'lecturer.index')->name('index');
+
+    // Dashboard lecturer // AKHTAR FATTAN WIDODO (5026231044)
+    Route::view('/dashboard', 'lecturer.dashboard.dashboard')->name('dashboard');
