f21066c867e9971f2794df90eea8d1dfc735f88c
Fezih <jinanfezih@gmail.com>
2025-11-16 17:35:48 +0700

    add routes mba Taffy

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShowController;
 
+/* -------------------------------------------------------------
+| STUDENT DASHBOARD | Taffy Nirarale Kamajaya - 5026221047
+|--------------------------------------------------------------*/
+Route::view('/student/dashboard', 'student.dashboard.dashboard')->name('student.dashboard');
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes : CREATED BY FEZIH SUHAIMAH JINAN 5026231055
